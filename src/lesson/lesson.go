476879784164f9c23317c2bdf5af9cc64528056a
claudiu <claudiu.garba@gmail.com>
2014-09-28 15:14:04 +0200

    lol

--- src/lesson/lesson.go
+++ src/lesson/lesson.go
@@ -9,4 +9,11 @@ import (
 func main() {
 	fmt.Printf("Hello world!\n")
 	fmt.Println(time.Now().Date())
+	s := make([]string, 3)
+	fmt.Println(s)
+	m := make(map[string] int)
+	m["a"] = 1
+	m["b"] = 2
+	m["c"] = 3
+	fmt.Println(m["a"])
 }
